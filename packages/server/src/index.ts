8978859dfa174d12955f4cabd7524ae2ed72f5ae
Manuel Gozzi <mannygozzidev@protonmail.com>
2024-05-11 20:03:47 -0700

    Added middleware for profiles page, no longer shows content for profile view

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import DestinationsRoute from "./routes/destinations";
 import ProfilesRoute from "./routes/profiles";
-import AuthRoute from "./routes/auth";
+import AuthRoute, { authenticateUser } from "./routes/auth";
 import path from "path";
 import { connect } from "./services/mongo";
 
@@ -19,7 +19,7 @@ const protoPublicDirectory = path.join(__dirname, "../../proto/public");
 app.use(express.json());
 app.use(express.static(protoPublicDirectory));
 app.use("/auth", AuthRoute);
-app.use("/api/profiles", ProfilesRoute);
+app.use("/api/profiles", authenticateUser, ProfilesRoute);
 app.use("/api/destinations", DestinationsRoute);
 
 app.listen(PORT, () => {
