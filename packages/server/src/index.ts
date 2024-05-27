66b7c52244c58b36fcc726bc87575c1008bc188a
Manuel Gozzi <mannygozzidev@protonmail.com>
2024-05-26 23:48:13 -0700

    Added new scripts and use route

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,13 +5,16 @@ import ProfilesRoute from "./routes/tours";
 import AuthRoute, { authenticateUser } from "./routes/auth";
 import path from "path";
 import { connect } from "./services/mongo";
+import fs from "node:fs/promises"
+import { Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
 const HOST_URL = process.env.HOST_URL;
 const PORT = process.env.PORT;
-
+const staticDir = path.resolve(__dirname, process.env.STATIC as string);
+console.log('Static Dir Path: ', staticDir)
 connect(process.env.MONGO_DB_NAME as string);
 
 /* Make non standard node_modules available */
@@ -26,6 +29,12 @@ app.use(express.static(protoPublicDirectory));
 app.use("/auth", AuthRoute);
 app.use("/api/tour", ProfilesRoute);
 app.use("/api/destinations", DestinationsRoute);
+app.use("/app", (_: Request, res: Response) => {
+  const indexHtml = path.resolve(staticDir, "index.html");
+  fs.readFile(indexHtml, { encoding: "utf8" }).then((html) =>
+    res.send(html)
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${HOST_URL}:${PORT} 🔥`);
